13af08d531fa9e938e3d64eb83340b03531bae5e
Carlos Ortiz <cortizmardones@gmail.com>
2019-05-22 17:13:11 -0400

    Avance con vistas+controladores

--- routes/web.php
+++ routes/web.php
@@ -15,6 +15,7 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('/prueba',function(){
-    return "Hola soy la ruta Prueba";
-});
+Route::get('/prueba','PruebaController@welcome');
+
+
+
