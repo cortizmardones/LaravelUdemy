15c95e587a16056bc9af7f1ac52e47a7173f3dd2
Carlos Ortiz <cortizmardones@gmail.com>
2019-05-23 12:47:15 -0400

    Ordene los articulos decentemente

--- resources/views/productos.blade.php
+++ resources/views/productos.blade.php
@@ -4,21 +4,35 @@
 
     <div class="container">
         <div class="row">
-            <div class="col-md-4"></div>
-            <div class="col-md-4">
-                
-                <h4 class="text-center">Nombre Producto: Precio:</h4>
-                
+            <div class="col-md-3">
+                <h4 class="text-center">N° Producto: </h4>
                 <hr>
                 @foreach ($products as $product)
-                    <h4 class="text-center"> {{$num = $num+1}} {{ $product->name}} {{ $product->price}} </h4>
-            
+                    <h4 class="text-center"> {{$num = $num+1}}</h4>
                 @endforeach
             </div>
-            <div class="col-md-4"></div>
-        </div>
-      
+            
+    
+            <div class="col-md-3">
+                <h4 class="text-center">Nombre Producto: </h4>
+                <hr>
+                @foreach ($products as $product)
+                    <h4 class="text-center"> {{ $product->name}}  </h4>
+                @endforeach
+            </div>
 
+            <div class="col-md-3">
+                <h4>Precio: </h4>
+                <hr>
+                @foreach ($products as $product)
+                <h4 class="text-center">{{ $product->price}}</h4>
+                @endforeach
+            </div>
+    
+
+
+            <div class="col-md-3"></div>
+        </div>
     </div>
 
 @endsection
